41f0a8390aa87b0856bfcb465e0d917ff161f40b
Onurgulec <032180043@ogr.uludag.edu.tr>
2022-01-06 16:10:20 +0300

    uygulamaya giriş kodları yazıldı
    Co-Authored-By: Berkay355916 <95080897+Berkay355916@users.noreply.github.com>

--- Kod.py
+++ Kod.py
@@ -1,1 +1,13 @@
 # sisteme giriş kodları #
+def giris(kullaniciadi , sifre ) :
+    while True :
+        x= input("kullanıcı adınızı giriniz: ")
+        y= input("şifrenizi giriniz ")
+        if x==kullaniciadi and y==sifre :
+            print(" ")
+            print(f"Giriş başarılı.>>>>>> Hoşgeldiniz {kullaniciadi}<<<<<<")
+            print(" ")
+            break
+        else:
+            print("Hatalı kullanıcı adı veya şifre.")
+            
