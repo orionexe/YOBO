d82e6b5a1bf19008c59cfb2836def040aba4104d
Berkay355916 <95080897+Berkay355916@users.noreply.github.com>
2022-01-06 16:44:26 +0300

    Uygulama altyapısı girildi
    Co-Authored-By: Onurgulec <032180043@ogr.uludag.edu.tr>

--- Kod.py
+++ Kod.py
@@ -11,3 +11,19 @@ def giris(kullaniciadi , sifre ) :
         else:
             print("Hatalı kullanıcı adı veya şifre.")
             
+def bakiyesorgu() :
+    print(ahmethesap["bakiye"])
+    ahmethesap={
+        "ad-soyad" : "Ahmet YILMAZ" ,
+        "kullanıciadi" : "ahmet16" ,
+        "sifre" :"1661" ,
+        "bakiye" : 5000
+    }
+
+    giris(ahmethesap["kullanıciadi"], ahmethesap["sifre"])
+
+    print("para çekmek için 1 ")
+    print("para yatırmak için 2 ")
+    print("bakiye sorgulamak için 3 ")
+    
+
